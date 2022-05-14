53b9a2ee60a56c17c8386d626b60fcccb5c21078
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-14 18:30:08 +0530

    Time: 20 ms (64.45%), Space: 9 MB (79.43%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int countSubsetSum(vector<int>arr, int sum, int n){
+        int t[n+1][sum+1];
+        for(int j=0;j<sum+1;j++) t[0][j] = 0;
+        for(int i=0;i<n+1;i++) t[i][0] = 1;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(arr[i-1] <= j) t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
+                else t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][sum];
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0;i<n;i++) sum += nums[i];
+        
+        if(sum < target || (sum + target) % 2 == 1 || (target + sum) < 0) return 0;
+        
+        int sum_s1 = (target + sum)/2;
+        return countSubsetSum(nums, sum_s1, n);
+    }
+};
