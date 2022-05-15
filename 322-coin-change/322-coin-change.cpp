61b6361bc5a7efbb92cadfb3589a7b832b27e1da
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-15 19:44:25 +0530

    Time: 111 ms (56.27%), Space: 10.5 MB (88.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -12,16 +12,22 @@ public:
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> t( n+1 , vector<int> (amount+1, -1));
-        for(int j=0;j<amount+1;j++) t[0][j] = INT_MAX - 1;
-        for(int i=0;i<n+1;i++) t[i][0] = 0;
+        int t[n+1][amount+1];  
+
+        for(int j=0; j <= amount; j++)
+            t[0][j] = amount+1;
+        for(int i=0; i <= n; i++)
+            t[i][0] = 0;
         
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<amount+1;j++){
-                if(coins[i-1]<=j) t[i][j] = min(1+t[i][j-coins[i-1]], t[i-1][j]);
-                else t[i][j] = t[i-1][j];
+        for(int i=1; i <= n; i++){
+            for(int j=1; j <= amount; j++) {
+                if(coins[i-1] <= j)
+                    t[i][j] = min(1 + t[i][j-coins[i-1]], t[i-1][j]);
+                else
+                    t[i][j] = t[i-1][j];
             }
         }
-        return t[n][amount] >= INT_MAX - 1 ? -1 : t[n][amount];
+        
+        return t[n][amount] > amount ? -1 : t[n][amount];
     }
 };
