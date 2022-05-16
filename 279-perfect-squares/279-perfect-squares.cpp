1a1b11361ff490027603363cbf3763583f661b6d
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-16 17:17:38 +0530

    Time: 108 ms (78.99%), Space: 6.7 MB (79.51%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
+    int dp[10001];
+    
+    int solve(int n){
+        if(n <= 0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int ans = INT_MAX;
+        for(int i=1;i*i<=n;i++) ans = min(ans, 1 + solve(n - i*i));
+        return dp[n] = ans;
+    }
     
     int numSquares(int n) {
-        vector<int>arr;
-        for(int i=1;i*i<=n;i++) arr.push_back(i*i);
-        // the problem no reduces to the unbounded knapsack.
-        int size = arr.size();
-        int t[size+1][n+1];
-        
-        for(int i=0;i<n+1;i++) t[0][i] = INT_MAX-1;
-        for(int i=0;i<size+1;i++) t[i][0] = 0;
-            
-        for(int i=1;i<size+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(arr[i-1] <= j) t[i][j] = min(1 + t[i][j-arr[i-1]], t[i-1][j]);
-                else t[i][j] = t[i-1][j];
-            }
-        }
-        return t[size][n];
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
