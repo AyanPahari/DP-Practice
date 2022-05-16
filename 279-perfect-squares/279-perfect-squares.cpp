e54a9e66c2766189c784b22706ac461c1208e9f6
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-16 17:17:43 +0530

    Time: 108 ms (78.99%), Space: 6.4 MB (81.48%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -3,7 +3,7 @@ public:
     int dp[10001];
     
     int solve(int n){
-        if(n <= 0) return 0;
+        if(n == 0) return 0;
         if(dp[n]!=-1) return dp[n];
         int ans = INT_MAX;
         for(int i=1;i*i<=n;i++) ans = min(ans, 1 + solve(n - i*i));
