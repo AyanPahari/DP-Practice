e348d83476fd0b1d852e7eab0cc507a19ad3d84f
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-16 20:24:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.30%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
@@ -13,14 +13,18 @@ public:
         // }
         // return dp[n] = ans;
         
-        // Time Complexity - O(n^2)
+        // Time Complexity - O(n)
         // Space Complexity - O(n)
-        dp[0] = 1, dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                if(j == 1 || j == 2) dp[i] = (dp[i] + dp[i - j]) % mod;
-                else dp[i] = (dp[i] % mod + 2 * dp[i - j] % mod) % mod;
-            }
+        // After applying some maths, the recurrence can be written as dp[i] = 2* dp[i-1] + dp[i-3]
+        dp[0] = 1, dp[1] = 1, dp[2] = 2;
+        // for(int i=2;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         if(j == 1 || j == 2) dp[i] = (dp[i] + dp[i - j]) % mod;
+        //         else dp[i] = (dp[i] % mod + 2 * dp[i - j] % mod) % mod;
+        //     }
+        // }
+        for(int i=3;i<=n;i++){
+            dp[i] = (2 * dp[i - 1] % mod + dp[i-3] % mod) % mod;
         }
         return dp[n];
     }
