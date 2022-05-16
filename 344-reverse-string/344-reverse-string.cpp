5497d18b269ef1e86666336f0edb878ab8e18af1
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-17 01:40:29 +0530

    Time: 19 ms (90.71%), Space: 23.2 MB (76.71%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     
-    void solve(vector<char>& s, int low, int high){
-        if(low > high) return;
-        swap(s[low],s[high]);
-        solve(s, low + 1, high - 1);
+    void solve(vector<char>& s, int low){
+        // if(low > high) return;
+        if(low >= s.size() - 1 - low) return;
+        swap(s[low],s[s.size() - 1 - low]);
+        solve(s, low + 1);
     }
     
     void reverseString(vector<char>& s) {
         int low = 0;
-        int high = s.size() - 1;
-        solve(s, low, high);
+        // int high = s.size() - 1;
+        // solve(s, low, high);
+        solve(s,low);
     }
 };
