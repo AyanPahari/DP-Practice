f8ac4e5690007eb041e70f6cfad6292f536059ea
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-17 21:41:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.52%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -5,12 +5,10 @@ public:
     int solve(int n){
         // if(n <= 1) return 1;
         // if(dp[n]!=-1) return dp[n];
-        dp[0] = 1, dp[1] = 1;
+        dp[1] = 1;
         
-        int ans = INT_MIN;
         for(int i=2;i<=n;i++){
-            for(int k=1;k<i;k++) ans = max(ans, k * max(dp[i - k], i - k));
-            dp[i] = ans;
+            for(int k=1;k<i;k++) dp[i] = max(dp[i], k * max(dp[i - k], i - k));
         }
         return dp[n];
     }
