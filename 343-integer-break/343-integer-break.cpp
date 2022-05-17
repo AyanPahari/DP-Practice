82d1a76ae759e6a6bf580f7b4c418dae254f1a3b
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-17 21:39:53 +0530

    Time: 3 ms (38.18%), Space: 5.9 MB (74.52%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -3,16 +3,20 @@ public:
     
     int dp[60];
     int solve(int n){
-        if(n <= 1) return 1;
-        if(dp[n]!=-1) return dp[n];
+        // if(n <= 1) return 1;
+        // if(dp[n]!=-1) return dp[n];
+        dp[0] = 1, dp[1] = 1;
         
         int ans = INT_MIN;
-        for(int k=1;k<n;k++) ans = max(ans, k * max(solve(n - k), n - k));
-        return dp[n] = ans;
+        for(int i=2;i<=n;i++){
+            for(int k=1;k<i;k++) ans = max(ans, k * max(dp[i - k], i - k));
+            dp[i] = ans;
+        }
+        return dp[n];
     }
     
     int integerBreak(int n) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         return solve(n);
     }
 };
