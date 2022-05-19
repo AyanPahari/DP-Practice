a5fe26354a09b917e4f030a188a67a01114c7e31
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-19 20:25:00 +0530

    Time: 13 ms (17.96%), Space: 7.9 MB (74.42%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     
-    bool checkpow2(int x){
-        return !(x & (x-1));
-    }
-    
     void solve(int n, vector<int>& dp){
-        dp[0] = 0, dp[1] = 1;
-        int last_pow = 1;
-        for(int i=2;i<=n;i++){
-            if(checkpow2(i)){
-                last_pow = i;
-                dp[i] = 1 + dp[0];
-            }
-            else dp[i] = 1 + dp[i - last_pow];
+        int offset = 1;
+        for(int i=1;i<=n;i++){
+            if(offset * 2 == i) offset = i;
+            dp[i] = 1 + dp[i - offset];
         }
     }
     
     vector<int> countBits(int n) {
-        if(n == 0) return {0};
         vector<int>dp(n + 1,0);
         solve(n, dp);
         return dp;
