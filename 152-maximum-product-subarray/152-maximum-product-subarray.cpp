17324d6a91a13d7616ea4ff4cf1e56803bd070f0
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-20 20:26:32 +0530

    Time: 19 ms (10.82%), Space: 14.3 MB (6.78%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int solve(vector<int>& nums, vector<pair<int,int>>& dp){
+        dp[0].first = nums[0];
+        dp[0].second = nums[0];
+        for(int i=1;i<nums.size();i++){
+            dp[i].first = max(nums[i], max(dp[i - 1].first * nums[i], dp[i - 1].second * nums[i]));
+            dp[i].second = min(nums[i], min(dp[i - 1].first * nums[i], dp[i - 1].second * nums[i]));
+        }
+        
+        int res = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            // cout<<dp[i].first<<" ";
+            res = max(res, dp[i].first);
+        }
+        return res;
+    }
+    
+    int maxProduct(vector<int>& nums) {
+        vector<pair<int,int>>dp(nums.size());
+        return solve(nums, dp);
+    }
+};
