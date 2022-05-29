0ef4d5ab7faf6801d07014ede4d7005c6fe71135
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-05-29 16:26:18 +0530

    Time: 3 ms (45.13%), Space: 5.8 MB (90.51%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     
+    int dp[1001];
+    int solve(int n){
+        if(n <= 1) return 0;
+        if(dp[n] != -1) return dp[n];
+        
+        for(int i=1;i<n;i++) if(n % i == 0 && solve(n - i) == 0) return dp[n] = 1; 
+        return dp[n] = 0;
+    }
+    
     bool divisorGame(int n) {
-        return n % 2 == 0;
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
     }
 };
