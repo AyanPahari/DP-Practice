d2a5adc6ff02f118acd3f4b47a8cfbe0cd96d877
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-01 17:16:16 +0530

    Time: 23 ms (49.22%), Space: 8.9 MB (48.44%) - LeetHub

---
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int dx[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+    int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+    
+    bool isValid(int row, int col, int n){
+        if(row >= n || col >= n || row < 0 || col < 0) return false;
+        return true;
+    }
+    
+    double solve(int row, int col, int n, int k, vector<vector<vector<double>>> &dp){
+        if(isValid(row, col, n) == false) return 0;
+        if(dp[row][col][k] != -1) return dp[row][col][k];
+        if(k == 0) return 1;
+        
+        double ans = 0;
+        for(int i=0;i<8;i++) ans += solve(row + dx[i], col + dy[i], n, k - 1, dp)/8.0;
+        return dp[row][col][k] = ans;
+    }
+    
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(n + 1, vector<vector<double>> (n + 1, vector<double>(k + 1, -1)));
+        return solve(row, column, n, k, dp);
+    }
+};
